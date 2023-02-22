b8147252b5fe6646021824f990ef8e40f3a3805d
sangeetha-ram <sangiammu1020@gmail.com>
2023-02-22 15:07:35 +0530

    Todo fetch for selected user

--- pages/user/Todo.js
+++ pages/user/Todo.js
@@ -1,14 +1,39 @@
 import { Button } from '@mui/material';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Todo() {
+  
+  const router = useRouter();
+const data = router.query;
+
+  const [todoList, setTodoList] = useState([])
+
+  async function todosListFn() {
+
+    const fetchtodosData= await fetch(`https://jsonplaceholder.typicode.com/todos?userId=${data.id}`) 
+    const todosData= await fetchtodosData.json()
+    setTodoList(todosData)
+  }
+
+  useEffect(() => {
+  todosListFn();
+
+  });
+
   return (
     <div>
-        <h4>user Todo</h4>
+        <h4>User Todo</h4>
         <h2>
-            <Button>
-                
-            <Link href="/">Back to the UserList</Link>
+          <ul>
+              {todoList?( todoList.map((item)=>
+              <li>* {item.title}</li>
+                )):'no todo found'}
+          </ul>
+            <Button>                
+
+              <Link href="/">Back to the todosList</Link>
             </Button>
         </h2>
     </div>
